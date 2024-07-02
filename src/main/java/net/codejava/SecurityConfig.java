b7452831af7d14a160ed433961eb57115d590fae
minj1992 <57328135+minj1992@users.noreply.github.com>
2024-07-03 00:22:53 +0530

    Create SecurityConfig.java

---
+++ src/main/java/net/codejava/SecurityConfig.java
@@ -0,0 +1,28 @@
+package net.codejava;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/register", "/process_register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll()
+                .and()
+            .headers()
+                .defaultsDisabled()
+                .cacheControl(); // Add this if you want to disable caching for security reasons
+    }
+}
